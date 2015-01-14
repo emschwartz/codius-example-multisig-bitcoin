9f11ae8b19153d70f2666f86a833932caf4f5847
Evan Schwartz <evan.mark.schwartz@gmail.com>
2015-01-14 17:44:31 +0100

    [TASK] Use TransactionBuilder to sign transactions

--- contract.js
+++ contract.js
@@ -1,36 +1,48 @@
+▽
 var secrets = require('secrets');
 var localstorage = require('localstorage');
+// localstorage exposes async getItem and setItem methods
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var Bitcoin = require('bitcoinjs-lib');
 var BigInteger = require('bigi');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('POST a JSON Bitcoin transaction in hex format to get it signed');
+  res.send('POST a Bitcoin transaction in hex format to get it signed');
 });
 
-app.post('/sign', bodyParser.json(), function(req, res) {
-  secrets.getKeypair(function(error, secret){
+app.get('/pubkey', function(req, res){
+  secrets.getKeypair(function(error, keypair){
+    res.send(keypair.public);
+  });  
+});
+
+app.post('/sign', bodyParser.text(), function(req, res) {
+ secrets.getKeypair(function(error, keypair){
     if (error) {
       res.status(500).send(error);
       return;
     }
+    console.log('Got request to sign tx');
 
     var key = new Bitcoin.ECKey(new BigInteger(keypair.private, 16), false);
 
     var tx, signature;
     try {
-      tx = Bitcoin.Transaction.fromHex(data);
-      signature = tx.sign(0, key);
+      tx = Bitcoin.Transaction.fromHex(req.body);
+      var txbuilder = Bitcoin.TransactionBuilder.fromTransaction(tx);
+      txbuilder.sign(0, key);
+      tx = txbuilder.build();
     } catch (e) {
-      res.status(400).send(error);
+      res.status(400).send(e.message);
       return;
     }
 
     res.status(200).json({
       public_key: keypair.public,
-      tx_signature: signature,
+      signed_tx: tx.toHex(),
       host_signature: keypair.signature
     });
   });
